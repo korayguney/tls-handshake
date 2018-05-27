7171b267ff8824c5b4ed5e6155c683f45add1f13
Koray Guney <kguney@netas.com.tr>
2018-05-27 13:22:54 +0300

    first commit

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,111 @@
+import com.nortelnetworks.ws.common.ProvisioningException;
+import com.nortelnetworks.ws.serverinfo.ServerInfoUserIF;
+import com.nortelnetworks.ws.serverinfo.ServerInfoUserServiceStub;
+import org.apache.axis.AxisProperties;
+import org.apache.axis.client.Stub;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+public class MainTest {
+
+    public static void main(String argv[]) {
+        A2SopiWSClient wsClient = new A2SopiServerInfoUserSvcWSClient();
+        System.out.println(makeWSCall("u1016@spidr.com", "1234", null, wsClient));
+
+    }
+
+    public static HashMap<String, Object> makeWSCall(String a2UserName,
+                                                     String a2Password, Object[] obj, A2SopiWSClient... wsClients) {
+        boolean allWSClientsDone = false;
+        boolean nextA2 = false;
+        boolean nextPA = false;
+        URL paAddress = null;
+        try {
+            paAddress = new URL("https://47.168.116.9:8043/sopi/services/ServerInfoUserService");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // TODO : set PA address
+        Object response = null;
+        A2SopiWSClient wsClient = wsClients[0];
+        HashMap<String, Object> responses = new HashMap<String, Object>();
+        // override SSL authentication if required
+
+        AxisProperties.setProperty("axis.socketSecureFactory",
+                "MyFakeTrustSocketFactory");
+        try {
+            response = wsClient.runService(paAddress, a2UserName, a2Password);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        responses.put(wsClient.getClass().getName(), response);
+
+        return responses;
+    }
+
+    interface A2SopiWSClient {
+        Object runService(URL paAddress, String a2UserName, String a2Password) throws ServiceException,
+                 RemoteException;
+    }
+
+    public static class A2SopiServerInfoUserSvcWSClient implements A2SopiWSClient {
+        // protected Logger logger = Logger.getLogger(this.getClass().getName());
+        private final String NAME = "ServerInfoUserService";
+
+        private final String HTTP = "http://";
+        private final String HTTPS = "https://";
+
+        /**
+         * Creates a new instance of A2SopiGABUserSvcWSClient
+         */
+        public A2SopiServerInfoUserSvcWSClient() {
+            super();
+        }
+
+
+
+
+        /**
+         * Get data from the PA
+         */
+
+        public Object runService(URL paAddress, String a2UserName, String a2Password) throws ServiceException,
+                 RemoteException {
+            ServerInfoUserServiceStub stub = new ServerInfoUserServiceStub(paAddress,null);
+           stub.setPortName("8043");
+            ServerInfoUserIF service = stub;
+            Stub.class.cast(service).setUsername(a2UserName);
+            Stub.class.cast(service).setPassword(a2Password);
+
+            Stub.class.cast(service).setHeader("SOPI", "version", "1.0");
+
+            Stub.class.cast(service)._setProperty(org.apache.axis.MessageContext.HTTP_TRANSPORT_VERSION,
+                    org.apache.axis.transport.http.HTTPConstants.HEADER_PROTOCOL_V11);
+            Stub.class.cast(service).setTimeout(5000);
+
+                System.out.println("Used A2 getServices to get assigned services for  " + a2UserName + " from "
+                        + paAddress.getHost() + ":" + paAddress.getPort());
+
+
+            int paPort = service.getSOAPServerData().getHttpsPort();
+            System.out.println("HTTPS port number : " + paPort);
+            String paHttpHeader = HTTPS;
+
+            if (paPort == 0) {
+                paPort = service.getSOAPServerData().getHttpPort();
+                System.out.println("HTTP port number : " + paPort);
+                paHttpHeader = HTTP;
+            }
+
+            return paHttpHeader + service.getSOAPServerData().getServerHostName() + ":" + paPort;
+        }
+
+    }
+}
---
+++ src/main/java/MyFakeTrustSocketFactory.java
@@ -0,0 +1,272 @@
+import org.apache.axis.components.logger.LogFactory;
+import org.apache.axis.components.net.BooleanHolder;
+import org.apache.axis.components.net.SecureSocketFactory;
+import org.apache.axis.components.net.TransportClientProperties;
+import org.apache.axis.components.net.TransportClientPropertiesFactory;
+import org.apache.axis.utils.Messages;
+import org.apache.axis.utils.StringUtils;
+import org.apache.axis.utils.XMLUtils;
+import org.apache.commons.logging.Log;
+import sun.security.provider.certpath.OCSP;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.Socket;
+import javax.net.ssl.*;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Hashtable;
+
+public class MyFakeTrustSocketFactory implements SecureSocketFactory {
+
+    /** Field log           */
+    protected static Log log =
+            LogFactory.getLog(MyFakeTrustSocketFactory.class.getName());
+    /**
+     * Constructor JSSESocketFactory
+     *
+     * @param attributes
+     */
+    public MyFakeTrustSocketFactory(Hashtable attributes) {
+
+    }
+
+    /**
+     * Method getContext
+     *
+     * @return
+     *
+     * @throws Exception
+     */
+    protected SSLContext getContext() throws Exception {
+
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+
+            sc.init(null, // we don't need no stinkin KeyManager
+                    new TrustManager[]{new MyFakeTrustSocketFactory.MyFakeX509TrustManager()},
+                    new java.security.SecureRandom());
+            if (log.isDebugEnabled()) {
+                log.debug(Messages.getMessage("My fake Socket Factory get context "));
+            }
+            return sc;
+        } catch (Exception exc) {
+            log.error(Messages.getMessage("My fake Socket Factory get context Exception"), exc);
+            throw new Exception(Messages.getMessage("ftsf02"));
+        }
+    }
+    protected SSLSocketFactory sslFactory = null;
+    protected void initFactory() throws IOException {
+        sslFactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
+    }
+    /**
+     * Create a socket
+     *
+     * @param host
+     * @param port
+     * @param otherHeaders
+     * @param useFullURL
+     * @return
+     * @throws Exception
+     */
+    public Socket create(String host, int port, StringBuffer otherHeaders, BooleanHolder useFullURL) throws Exception {
+        SSLContext sslContext = javax.net.ssl.SSLContext.getInstance("TLS","BCJSSE");
+        String kf = "D:\\spidr1.truststore";
+     //   String keyStoreFilePath = "C:\\Program Files\\Java\\jdk1.8.0_144\\jre\\lib\\security\\cacerts";
+        String keyStoreFilePassword = "changeit";
+        File keystoreFile = new File(kf);
+        if(!keystoreFile.exists() || keystoreFile.isDirectory())
+            return null;
+
+        KeyStore keyStore = KeyStore.getInstance("JKS", "SUN");
+        FileInputStream fin = new FileInputStream(kf);
+        keyStore.load(fin, keyStoreFilePassword.toCharArray());
+
+       /* final LiveCertX509TrustManager tm = new LiveCertX509TrustManager();
+        final TrustManager[] tms = new TrustManager[] { tm };*/
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        tmf.init(keyStore);
+
+        sslContext.init(null,
+                tmf.getTrustManagers(),
+                new java.security.SecureRandom() );
+        sslFactory = sslContext.getSocketFactory();
+        if (port == -1) {
+            port = 443;
+        }
+
+        TransportClientProperties tcp = TransportClientPropertiesFactory.create("https");
+
+        //boolean hostInNonProxyList = isHostInNonProxyList(host, tcp.getNonProxyHosts());
+        boolean hostInNonProxyList = false;
+        Socket sslSocket = null;
+        if (tcp.getProxyHost().length() == 0 || hostInNonProxyList) {
+            // direct SSL connection
+            sslSocket = sslFactory.createSocket(host, port);
+        }
+        else {
+
+            // Default proxy port is 80, even for https
+            int tunnelPort = (tcp.getProxyPort().length() != 0)
+                    ? Integer.parseInt(tcp.getProxyPort())
+                    : 80;
+            if (tunnelPort < 0)
+                tunnelPort = 80;
+
+            // Create the regular socket connection to the proxy
+            Socket tunnel = new Socket(tcp.getProxyHost(), tunnelPort);
+
+            // The tunnel handshake method (condensed and made reflexive)
+            OutputStream tunnelOutputStream = tunnel.getOutputStream();
+            PrintWriter out = new PrintWriter(
+                    new BufferedWriter(new OutputStreamWriter(tunnelOutputStream)));
+
+            // More secure version... engage later?
+            // PasswordAuthentication pa =
+            // Authenticator.requestPasswordAuthentication(
+            // InetAddress.getByName(tunnelHost),
+            // tunnelPort, "SOCK", "Proxy","HTTP");
+            // if(pa == null){
+            // printDebug("No Authenticator set.");
+            // }else{
+            // printDebug("Using Authenticator.");
+            // tunnelUser = pa.getUserName();
+            // tunnelPassword = new String(pa.getPassword());
+            // }
+            out.print("CONNECT " + host + ":" + port + " HTTP/1.0\r\n"
+                    + "User-Agent: AxisClient");
+            if (tcp.getProxyUser().length() != 0 &&
+                    tcp.getProxyPassword().length() != 0) {
+
+                // add basic authentication header for the proxy
+                String encodedPassword = XMLUtils.base64encode((tcp.getProxyUser()
+                        + ":"
+                        + tcp.getProxyPassword()).getBytes());
+
+                out.print("\nProxy-Authorization: Basic " + encodedPassword);
+            }
+            out.print("\nContent-Length: 0");
+            out.print("\nPragma: no-cache");
+            out.print("\r\n\r\n");
+            out.flush();
+            InputStream tunnelInputStream = tunnel.getInputStream();
+
+            if (log.isDebugEnabled()) {
+                log.debug(Messages.getMessage("isNull00", "tunnelInputStream",
+                        "" + (tunnelInputStream
+                                == null)));
+            }
+            String replyStr = "";
+
+            // Make sure to read all the response from the proxy to prevent SSL negotiation failure
+            // Response message terminated by two sequential newlines
+            int newlinesSeen = 0;
+            boolean headerDone = false;    /* Done on first newline */
+
+            while (newlinesSeen < 2) {
+                int i = tunnelInputStream.read();
+
+                if (i < 0) {
+                    throw new IOException("Unexpected EOF from proxy");
+                }
+                if (i == '\n') {
+                    headerDone = true;
+                    ++newlinesSeen;
+                } else if (i != '\r') {
+                    newlinesSeen = 0;
+                    if (!headerDone) {
+                        replyStr += String.valueOf((char) i);
+                    }
+                }
+            }
+            if (StringUtils.startsWithIgnoreWhitespaces("HTTP/1.0 200", replyStr) &&
+                    StringUtils.startsWithIgnoreWhitespaces("HTTP/1.1 200", replyStr)) {
+                throw new IOException(Messages.getMessage("cantTunnel00",
+                        new String[]{
+                                tcp.getProxyHost(),
+                                "" + tunnelPort,
+                                replyStr}));
+            }
+
+            // End of condensed reflective tunnel handshake method
+            sslSocket = sslFactory.createSocket(tunnel, host, port, true);
+            if (log.isDebugEnabled()) {
+                log.debug(Messages.getMessage("setupTunnel00",
+                        tcp.getProxyHost(),
+                        "" + tunnelPort));
+            }
+        }
+
+        ((SSLSocket) sslSocket).startHandshake();
+        if (log.isDebugEnabled()) {
+            log.debug(Messages.getMessage("createdSSL00"));
+        }
+        return sslSocket;
+    }
+
+    /**
+     * Class FakeX509TrustManager
+     */
+    public static class MyFakeX509TrustManager implements X509TrustManager {
+
+        /** Field log           */
+        protected static Log log =
+                LogFactory.getLog(MyFakeTrustSocketFactory.MyFakeX509TrustManager.class.getName());
+
+        /**
+         * Method isClientTrusted
+         *
+         * @param chain
+         *
+         * @return
+         */
+        public boolean isClientTrusted(java.security.cert
+                                               .X509Certificate[] chain) {
+
+            if (log.isDebugEnabled()) {
+                log.debug(Messages.getMessage("my fake trust manager is client trusted"));
+            }
+            return true;
+        }
+
+        /**
+         * Method isServerTrusted
+         *
+         * @param chain
+         *
+         * @return
+         */
+        public boolean isServerTrusted(java.security.cert
+                                               .X509Certificate[] chain) {
+
+            if (log.isDebugEnabled()) {
+                log.debug(Messages.getMessage("my fake trust manager is server trusted"));
+            }
+            return true;
+        }
+        //TODO :
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            return;
+        }
+        //TODO :
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            return;
+        }
+
+        /**
+         * Method getAcceptedIssuers
+         *
+         * @return
+         */
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+
+            if (log.isDebugEnabled()) {
+                log.debug(Messages.getMessage("My Fake Trust manager get accepted issuers"));
+            }
+            return null;
+        }
+    }
+}
